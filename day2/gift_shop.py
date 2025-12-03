3caf3b6d28de4416270bcaabadcc7caf104530d4
Laura Reese <laurareese03@gmail.com>
2025-12-02 18:18:37 -0600

    day 2 added timing and made it faster

--- day2/gift_shop.py
+++ day2/gift_shop.py
@@ -1,4 +1,5 @@
-import textwrap
+import textwrap, time
+start_time = time.time()
 
 id_ranges = open('product_ids.txt').read().replace('\n', '').split(',')
 
@@ -14,8 +15,11 @@ for id_range in id_ranges:
     # part b
     # chunk the id into equal length strings and check if all are the same
     # I wonder how much faster I can make it without checking the 1 factor
+    # (about 26 seconds, not awful)
     for j in range (1, (len(i)//2)+1):
       if len(i)%j == 0:
+        # if j == 1 and i[0] != i[1]:
+        #   continue
         holder = textwrap.wrap(i, j)
         if len(set(holder)) == 1:
           count_b += int(i)
@@ -23,3 +27,4 @@ for id_range in id_ranges:
 
 print(count_a)
 print(count_b)
+print(time.time()-start_time)
