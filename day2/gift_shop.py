ab50c95472edb0118ad72632092e2cfe6156f9b1
Laura Reese <laurareese03@gmail.com>
2025-12-02 22:10:06 -0600

    day 2 ohhhhh you gotta be quicker than that

--- day2/gift_shop.py
+++ day2/gift_shop.py
@@ -3,6 +3,19 @@ start_time = time.time()
 
 id_ranges = open('product_ids.txt').read().replace('\n', '').split(',')
 
+coprime_factors = {
+  1: [],
+  2: [1],
+  3: [1],
+  4: [2],
+  5: [1],
+  6: [2,3],
+  7: [1],
+  8: [4],
+  9: [3],
+  10: [2, 5]
+}
+
 count_a, count_b = 0, 0 
 for id_range in id_ranges:
   r = id_range.split('-')
@@ -15,15 +28,12 @@ for id_range in id_ranges:
     # part b
     # chunk the id into equal length strings and check if all are the same
     # I wonder how much faster I can make it without checking the 1 factor
-    # (about 26 seconds, not awful)
-    for j in range (1, (len(i)//2)+1):
-      if len(i)%j == 0:
-        # if j == 1 and i[0] != i[1]:
-        #   continue
-        holder = textwrap.wrap(i, j)
-        if len(set(holder)) == 1:
-          count_b += int(i)
-          break
+    # (dropped from 67s to 41s to 31s only checking needed duplicates)
+    for j in coprime_factors[len(i)]:
+      holder = textwrap.wrap(i, j)
+      if len(set(holder)) == 1:
+        count_b += int(i)
+        break
 
 print(count_a)
 print(count_b)
