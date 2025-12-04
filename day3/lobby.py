c6094c46b704199aa09b7cb742a57735df0719f1
Laura Reese <laurareese03@gmail.com>
2025-12-03 18:53:31 -0600

    day 3 more general solution for both parts

--- day3/lobby.py
+++ day3/lobby.py
@@ -5,21 +5,22 @@ ratings = open('joltage_ratings.txt').read().split('\n')
 
 def get_next_highest_digit(rating, length):
   for i in range(9, 0 , -1):
-    print(rating, rating[:length])
     ind = rating[:length].find(str(i))
     if ind > -1:
       return ind
+    
+def calculate_joltage(rating, length):
+    joltage = ''
+    while len(joltage) < length:
+      ind = get_next_highest_digit(rating, len(rating) - (length-len(joltage)) +1)
+      joltage += str(rating[ind])
+      rating = rating[ind+1:]
+    return int(joltage)
 
 count_a, count_b = 0, 0
 for rating in ratings:
-  joltage = ''
-
-  while len(joltage) < 12:
-    ind = get_next_highest_digit(rating, len(rating) - (12-len(joltage)) +1)
-    joltage += str(rating[ind])
-    rating = rating[ind+1:]
-
-  count_b += int(joltage)
+  count_a += calculate_joltage(rating, 2)
+  count_b += calculate_joltage(rating, 12)
 
 print(count_a)
 print(count_b)
