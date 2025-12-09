f184acdb20458a629798d2c0850082025dbd4626
Laura Reese <laurareese03@gmail.com>
2025-12-08 20:22:04 -0600

    day 8 part b i love combining parts like this

--- day8/playground.py
+++ day8/playground.py
@@ -13,25 +13,40 @@ for l in locations:
 def get_distance(x, y):
   return math.sqrt((y[0] - x[0]) ** 2 + (y[1] - x[1]) ** 2 + (y[2] - x[2]) ** 2)
 
+def print_top_circuits():
+  circuit_sizes = []
+  visited = set()
+  for r in rounds:
+    if r not in visited:
+      visited = visited.union(rounds[r])
+      circuit_sizes.append(len(rounds[r]))
+
+  sorted_sizes = sorted(circuit_sizes, reverse=True)
+  print(sorted_sizes[0] * sorted_sizes[1] * sorted_sizes[2])
+
 weights = q.PriorityQueue()
 for i in range(len(locations)-1):
   for j in range(i+1, len(locations)):
     weights.put((get_distance(locations[i], locations[j]), [locations[i], locations[j]]))
 
-for i in range(1000):
+i = 0
+while not weights.empty():
+  # part a
+  if i == 1000:
+    print_top_circuits()
+
   w = weights.get()
   pair = w[1]
   if pair[0] not in rounds[pair[1]]:
     circuit = rounds[pair[0]].union(rounds[pair[1]])
+
+    # part b
+    if len(circuit) == len(locations):
+      print(pair[0][0] * pair[1][0])
+      break
     for c in circuit:
       rounds[c] = circuit
 
-circuit_sizes = []
-visited = set()
-for r in rounds:
-  if r not in visited:
-    visited = visited.union(rounds[r])
-    circuit_sizes.append(len(rounds[r]))
+  i += 1
 
-sorted_sizes = sorted(circuit_sizes, reverse=True)
-print(sorted_sizes[0] * sorted_sizes[1] * sorted_sizes[2])
+print(time.time() - start_time)
