ccfd118fca7f6a1dc023d5ae491bb345a736c71f
Laura Reese <laurareese03@gmail.com>
2025-12-11 21:50:20 -0600

    day 11 cleanup

--- day11/reactor.py
+++ day11/reactor.py
@@ -21,22 +21,12 @@ def dfs(current, success, failure):
   return visited[current]
 
 # part a
-count = 0
-frontier = q.Queue()
-frontier.put('you')
-while not frontier.empty():
-  current = frontier.get()
-  if current == 'out':
-    count += 1
-  else:
-    for f in device_map[current]:
-      frontier.put(f)
-print(count)
+visited = {}
+print(dfs('you', 'out', []))
 
 # part b
 # i love the advent of code but i HATE when the advent of code makes me write
 # search algorithms recursively.
-
 # the thought here at least (that does work) is we count the number of paths between
 # dac and out, then the number of paths between fft and dac, and finally the number
 # of paths between svr and fft, then multiply the three values together to get the solution
@@ -47,3 +37,5 @@ b = dfs('fft', 'dac', ['out'])
 visited = {}
 c = dfs('svr', 'fft', ['dac', 'out'])
 print(a*b*c)
+
+print(time.time() - start_time)
